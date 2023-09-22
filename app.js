37d3397769d88b8b0e02866a18138db309450123
Adam Thometz <adamthometz@gmail.com>
2023-09-22 16:18:36 -0400

    changed font, color, and scale

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ const playIcon = '<i class="fa-solid fa-play" style="color: #ffffff;"></i>';
 const pauseIcon = '<i class="fa-solid fa-pause" style="color: #ffffff;"></i>';
 
 const SCALES = {
-  keys: 3,
-  synths: 3,
-  bass: 1.5,
-  drums: 10,
+  keys: 1.5,
+  synths: 1.5,
+  bass: 0.5,
+  drums: 5,
 };
 
 let animationIds = AUDIO_BLOCKS.reduce(function createIdsObj(accum, val) {
